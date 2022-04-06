fb2345bb5174e2dfc27dd56d17f13e51729ef4fa
SihanChen <SIHANCHEN@sfmail.sf-express.com>
2022-04-07 07:39:37 +0800

    新增链表节点

---
+++ app/src/main/java/com/sfexpress/vcleet/bean/ListNode.java
@@ -0,0 +1,16 @@
+package com.sfexpress.vcleet.bean;
+
+/**
+ * 链表节点
+ *
+ * @author veganchen
+ * @since 2022/4/7 07:38
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
